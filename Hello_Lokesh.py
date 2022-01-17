ac5c0b4726a8f9644ff8232a8087fd577da4edd5
LokeshBokkisam-data <bokkisamlokeshchowdary@gmail.com>
2022-01-17 13:10:44 -0500

    I edited both python and gitignore files

--- Hello_Lokesh.py
+++ Hello_Lokesh.py
@@ -7,17 +7,17 @@ print(10//3, 3//10) # print quotients from divisions
 print(10%3, 3%10) # print remainders from divisions
 
 #%%
-astring = "Thank you"
+astring = "Lokeshbokkisam"
 anum = 3.14159265358979323846
 cnt = 1
 # Many different ways to print out the same line
-print("%d. I want to say %s" % (cnt,astring) )
+print("%d. My name is %s" % (cnt,astring) )
 cnt+=1
-print(cnt,". I want to say" + astring )
+print(cnt,". u can call me " + astring )
 cnt+=1
 print(cnt, ". I want to say",astring )
 cnt+=1
-print("%d. I want to say %s, my sweetie %.3f" % (cnt,astring,anum) )
+print("%d. I want to say %s, my fav number %.3f" % (cnt,astring,anum) )
 cnt+=1
 print("%d. I want to say %s, my sweetie digit %d" % (cnt,astring,anum) )
 cnt+=1
